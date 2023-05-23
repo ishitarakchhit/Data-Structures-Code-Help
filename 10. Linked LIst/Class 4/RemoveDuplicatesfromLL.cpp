9d2204ff4aa83d877ccd0f938313d88f988ea8ef
ishita067btmae21@igdtuw.ac.in <104856237+ishitarakchhit@users.noreply.github.com>
2023-05-23 15:06:24 +0530

    adding RemoveDuplicatesfromLL.cpp in class 4LL

--- 10. Linked LIst/Class 4/RemoveDuplicatesfromLL.cpp
+++ 10. Linked LIst/Class 4/RemoveDuplicatesfromLL.cpp
@@ -37,13 +37,42 @@ void printll(node* &head){
 }
 
 
+//function to remove duplicates from linked list
+void removeduplicates(node* &head){
+
+    if(head== NULL || head->next==NULL){
+        return;
+    }
+    node* curr = head;
+    while(curr!=NULL){
+        if(curr->next == NULL){
+            break;
+        }
+
+        if(curr->data == curr->next->data){           //if elements are repeating then delete the repeating element and coneect the previous node to the next node
+            node* temp = curr->next;
+            curr->next = curr->next->next;
+            temp->next =NULL;
+            delete temp;
+        }
+
+        else{                                         //if elements are not repeating iterate over the next node
+            curr = curr->next;
+        }
+    }
+    return;
+}
+
 //main function
 int main(){
-    node* first = new node(4);
-    node* head = first;
+    node* head = new node(4);
     
     insertatbeg(head, 4);
     insertatbeg(head, 4);
+    insertatbeg(head, 4);
+    insertatbeg(head, 4);
+    insertatbeg(head, 4);
+    insertatbeg(head, 3);
     insertatbeg(head, 3);
     insertatbeg(head, 3);
     insertatbeg(head, 2);
@@ -51,6 +80,11 @@ int main(){
     insertatbeg(head, 2);
     insertatbeg(head, 1);
 
+    printll(head);
+
+    cout<<endl;
+
+    removeduplicates(head);                             //function call to remove duplicates from linked list
     printll(head);
 
    
