1bf226e206d4ef5bf25fd158f29899f7394dbbe8
ishita067btmae21@igdtuw.ac.in <104856237+ishitarakchhit@users.noreply.github.com>
2023-06-17 23:54:08 +0530

    adding PathSumII.cpp

---
+++ 13. Trees/Class 2/PathSumII.cpp
@@ -0,0 +1,73 @@
+/*
+Given the root of a binary tree and an integer targetSum, return all root-to-leaf paths where the sum of the node values in the path equals targetSum. Each path should be returned as a list of the node values, not node references.
+
+A root-to-leaf path is a path starting from the root and ending at any leaf node. A leaf is a node with no children.
+
+ 
+
+Example 1:
+
+
+Input: root = [5,4,8,11,null,13,4,7,2,null,null,5,1], targetSum = 22
+Output: [[5,4,11,2],[5,8,4,5]]
+Explanation: There are two paths whose sum equals targetSum:
+5 + 4 + 11 + 2 = 22
+5 + 8 + 4 + 5 = 22
+Example 2:
+
+
+Input: root = [1,2,3], targetSum = 5
+Output: []
+Example 3:
+
+Input: root = [1,2], targetSum = 0
+Output: []
+ 
+
+Constraints:
+
+The number of nodes in the tree is in the range [0, 5000].
+-1000 <= Node.val <= 1000
+-1000 <= targetSum <= 1000
+*/
+
+
+class Solution {
+    bool checkSum(vector<int> path, int targetSum){
+        int sum =0;
+        for(int i=0; i<path.size(); i++){
+            sum = sum+path[i];
+        }
+        return sum == targetSum;
+    }
+
+    void solve(TreeNode* root, int targetSum, vector<vector<int>> &ans, vector<int> &path){
+        if(root == NULL){
+            
+            return;
+        }
+
+        path.push_back(root->val);
+        if(root->left == NULL && root->right == NULL && checkSum(path, targetSum)){
+        ans.push_back(path);
+    }
+        solve(root->left, targetSum, ans, path);
+            solve(root->right, targetSum, ans, path);
+            path.pop_back();
+        
+            
+
+
+    }
+public:
+    vector<vector<int>> pathSum(TreeNode* root, int targetSum) {
+        vector<vector<int>> ans;
+        vector<int> path;
+        if(root == NULL && targetSum ==0){
+            return ans;
+        }
+
+        solve(root, targetSum, ans, path);
+        return ans;
+    }
+};
