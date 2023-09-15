0625bd0d53656551c03157796340ba6749ef2ba2
ishita067btmae21@igdtuw.ac.in <104856237+ishitarakchhit@users.noreply.github.com>
2023-09-15 23:59:19 +0530

    adding files in Graphs

---
+++ 18. Graphs/13. DistanceOfNearest0.cpp
@@ -0,0 +1,88 @@
+/*
+Given a binary grid of n*m. Find the distance of the nearest 1 in the grid for each cell.
+The distance is calculated as |i1  - i2| + |j1 - j2|, where i1, j1 are the row number and column number of the current cell, and i2, j2 are the row number and column number of the nearest cell having value 1. There should be atleast one 1 in the grid.
+ 
+
+Example 1:
+
+Input: grid = {{0,1,1,0},{1,1,0,0},{0,0,1,1}}
+Output: {{1,0,0,1},{0,0,1,1},{1,1,0,0}}
+Explanation: The grid is-
+0 1 1 0 
+1 1 0 0 
+0 0 1 1 
+0's at (0,0), (0,3), (1,2), (1,3), (2,0) and
+(2,1) are at a distance of 1 from 1's at (0,1),
+(0,2), (0,2), (2,3), (1,0) and (1,1)
+respectively.
+
+
+Example 2:
+
+Input: grid = {{1,0,1},{1,1,0},{1,0,0}}
+Output: {{0,1,0},{0,0,1},{0,1,2}}
+Explanation: The grid is-
+1 0 1
+1 1 0
+1 0 0
+0's at (0,1), (1,2), (2,1) and (2,2) are at a 
+distance of 1, 1, 1 and 2 from 1's at (0,0),
+(0,2), (2,0) and (1,1) respectively.
+
+
+ 
+
+Yout Task:
+You don't need to read or print anything, Your task is to complete the function nearest() which takes the grid as an input parameter and returns a matrix of the same dimensions where the value at index (i, j) in the resultant matrix signifies the minimum distance of 1 in the matrix from grid[i][j].
+ 
+
+Expected Time Complexity: O(n*m)
+Expected Auxiliary Space: O(n*m)
+
+Constraints:
+1 ≤ n, m ≤ 500
+*/
+
+
+
+vector<vector<int>> updateMatrix(vector<vector<int>>& mat) {
+        int n = mat.size();
+        int m = mat[0].size();
+        vector<vector<int>> ans(n, vector<int> (m, 0));
+        vector<vector<int>> vis(n, vector<int> (m, 0));
+        queue<pair<pair<int,int>, int>> q;
+
+
+        for(int i=0; i<n; i++){
+            for(int j=0; j<m; j++){
+                if(mat[i][j] == 1){
+                    vis[i][j] = 1;
+                    q.push({{i, j}, 0});
+                }
+            }
+        }
+
+        int dx[] = {-1, 0, 1, 0};
+        int dy[] = {0, 1, 0, -1};
+        while(!q.empty()){
+            int row = q.front().first.first;
+            int col = q.front().first.second;
+            int time = q.front().second;
+            q.pop();
+            ans[row][col] = time;
+
+            //for traversing dx dy
+            for(int i=0; i<4; i++){
+                int nx = row + dx[i];
+                int ny = col + dy[i];
+                if(nx>=0 && nx<n && ny>=0 && ny<m && vis[nx][ny] == 0){
+                    q.push({{nx, ny}, time+1});
+                    vis[nx][ny] = 1;
+                }
+            }
+
+        }
+
+
+        return ans;
+    }
